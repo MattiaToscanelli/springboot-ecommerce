291957e8c96b3a0975e334d41357184f7006358a
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-02-23 16:53:28 +0100

    feat: added project base with simple GET request methods

---
+++ src/main/java/ch/toscanelli/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,15 @@
+package ch.toscanelli.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//./mvnw spring-boot:run --> FIX Loombook
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
