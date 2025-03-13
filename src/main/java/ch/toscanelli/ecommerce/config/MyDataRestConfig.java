32a0b238e7d63d8d5f53fb8fa8bda358937a2e63
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-03-13 21:30:04 +0100

    feat: added find ProductCategory by Id

--- src/main/java/ch/toscanelli/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/ch/toscanelli/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,22 @@ package ch.toscanelli.ecommerce.config;
 
 import ch.toscanelli.ecommerce.entity.Product;
 import ch.toscanelli.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
@@ -24,5 +32,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // get a list of all entity classes from the entity manager
+        var entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        var entityClasses = entities.stream()
+                .map(Type::getJavaType)
+                .toArray(Class[]::new);
+
+        // expose the entity ids for the array of entity/domain types
+        config.exposeIdsFor(entityClasses);
     }
 }
